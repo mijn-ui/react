b72d8defc342bff81cbe011fa701f4e8f22b1689
HTLA380 <htetlin099798@gmail.com>
2024-10-15 19:17:57 +0630

    chore: testing eslint config

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -14,6 +14,7 @@ export const Button = ({ children, className, appName }: ButtonProps) => {
       className={className}
       onClick={() => alert(`Hello from your ${appName} app!`)}
     >
+      test
       {children}
     </button>
   )
