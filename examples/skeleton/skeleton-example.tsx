222e4c71605a13a49393b70781619adf38e4b377
HTLA380 <htetlin099798@gmail.com>
2024-11-07 17:33:20 +0630

    docs: add skeleton docs

--- examples/skeleton/skeleton-example.tsx
+++ examples/skeleton/skeleton-example.tsx
@@ -1,9 +1,9 @@
 import { Skeleton } from "@mijn-ui/components/skeleton"
 
-export function SkeletonCard() {
+const SkeletonExample = () => {
   return (
-    <div className="flex flex-col space-y-3">
-      <Skeleton className="h-[125px] w-[250px] rounded-xl" />
+    <div className="flex items-center space-x-4">
+      <Skeleton className="h-12 w-12 rounded-full" />
       <div className="space-y-2">
         <Skeleton className="h-4 w-[250px]" />
         <Skeleton className="h-4 w-[200px]" />
@@ -11,3 +11,5 @@ export function SkeletonCard() {
     </div>
   )
 }
+
+export default SkeletonExample
