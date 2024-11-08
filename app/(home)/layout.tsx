ebf1b3eccf200708dc0baa627d409470a4c702d4
HTLA380 <htetlin099798@gmail.com>
2024-11-08 19:13:16 +0630

    style: fix home page layout issues

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,6 +1,11 @@
 import type { ReactNode } from "react"
+import { Metadata } from "next"
 import Footer from "./footer"
 import Navbar from "./navbar"
+
+export const metadata: Metadata = {
+  description: "Reusable components for clean, responsive interfaces.",
+}
 
 export default function Layout({
   children,
@@ -8,10 +13,10 @@ export default function Layout({
   children: ReactNode
 }): React.ReactElement {
   return (
-    <>
+    <main className="flex w-full flex-col items-center justify-center h-screen">
       <Navbar />
       {children}
       <Footer />
-    </>
+    </main>
   )
 }
