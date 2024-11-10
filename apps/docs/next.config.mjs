0d89ad397991833b59ffd12a4d7cb9f300ef7d09
HTLA380 <htetlin099798@gmail.com>
2024-11-10 20:13:44 +0630

    chore: update next config file

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -1,12 +1,20 @@
 import { createMDX } from "fumadocs-mdx/next"
+import path from "path"
+import { fileURLToPath } from "url"
 
 const withMDX = createMDX()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
   resolve: {
     extensions: [".mjs", ".js", ".jsx", ".json"],
+  },
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
   },
 }
 
