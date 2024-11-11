7cf2a095774682dc542889ee353aff1b40c647a8
HTLA380 <htetlin099798@gmail.com>
2024-11-11 19:35:58 +0630

    ci: add lint check and resolve issues

--- packages/mijn-ui/components/progress/progress.tsx
+++ packages/mijn-ui/components/progress/progress.tsx
@@ -1,16 +1,29 @@
-"use client";
+"use client"
 
-import * as React from "react";
-import { UnstyledProps } from "@mijn-ui/types";
-import { applyUnstyled } from "@mijn-ui/utils";
-import * as ProgressPrimitive from "@radix-ui/react-progress";
+import * as React from "react"
+import { UnstyledProps } from "@mijn-ui/types"
+import { applyUnstyled } from "@mijn-ui/utils"
+import * as ProgressPrimitive from "@radix-ui/react-progress"
 
-type ProgressProps = React.ComponentPropsWithRef<typeof ProgressPrimitive.Root> & UnstyledProps;
+type ProgressProps = React.ComponentPropsWithRef<
+  typeof ProgressPrimitive.Root
+> &
+  UnstyledProps
 
-const Progress = ({ className, unstyled, value, ref, ...props }: ProgressProps) => (
+const Progress = ({
+  className,
+  unstyled,
+  value,
+  ref,
+  ...props
+}: ProgressProps) => (
   <ProgressPrimitive.Root
     ref={ref}
-    className={applyUnstyled(unstyled, "relative h-2 w-full overflow-hidden rounded-full bg-neutral", className)}
+    className={applyUnstyled(
+      unstyled,
+      "relative h-2 w-full overflow-hidden rounded-full bg-neutral",
+      className,
+    )}
     {...props}
   >
     <ProgressPrimitive.Indicator
@@ -18,6 +31,6 @@ const Progress = ({ className, unstyled, value, ref, ...props }: ProgressProps) 
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
-);
+)
 
-export { Progress };
+export { Progress }
