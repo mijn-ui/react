960fd39e1e94c1dd3748805433fc51fabaf5785a
HTLA380 <htetlin099798@gmail.com>
2024-11-11 15:18:19 +0630

    ci: add github actions

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -13,6 +13,10 @@ const config = {
   resolve: {
     extensions: [".mjs", ".js", ".jsx", ".json"],
   },
+
+  // I have to specify this to avoid an error during the production build with turbo.
+  // I don't know exactly what the problem is, but it may have something to do with building the cache locally.
+  // You can find more information about this problem and its discussion on GitHub at "https://github.com/vercel/next.js/discussions/39432".
   experimental: {
     outputFileTracingRoot: path.join(__dirname, "../../"),
   },
