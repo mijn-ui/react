f9b3a37ca3d7f5f8f0e2238021b58db3712b2f4f
HTLA380 <htetlin099798@gmail.com>
2024-11-13 21:58:45 +0630

    feat: remove every files to create a new version
    I am removing all files to create a new version of the project due to inconsistent dependencies and a problematic previous mono repo setup.

--- packages/mijn-ui/__test__/unstyled-provider.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, it } from "vitest"
-
-describe("group", () => {
-  it("should", () => {
-    expect(2).toBeTruthy()
-  })
-})
--- packages/mijn-ui/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineProject } from "vitest/config"
-
-export default defineProject({
-  resolve: {
-    alias: {
-      "@mijn-ui/": new URL("./src/", import.meta.url).pathname,
-    },
-  },
-})
