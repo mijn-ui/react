535b425d8faf7916cf7273c31535e75472edfdbc
HTLA380 <htetlin099798@gmail.com>
2024-11-15 13:16:56 +0630

    chore(docs): update tailwind config

--- apps/docs/tailwind.config.js
+++ apps/docs/tailwind.config.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import { createPreset } from "fumadocs-ui/tailwind-plugin";
 
 /** @type {import('tailwindcss').Config} */
@@ -7,7 +9,10 @@ export default {
     "./app/**/*.{ts,tsx}",
     "./content/**/*.{md,mdx}",
     "./mdx-components.{ts,tsx}",
+
+    // I don't know why this path isn't working in production, so I have to include it this way for development and production
     "../../node_modules/fumadocs-ui/dist/**/*.js",
+    "./node_modules/fumadocs-ui/dist/**/*.js",
   ],
   presets: [createPreset()],
 };
