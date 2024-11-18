5bdb39272d05af7a4831903c0e331bd336038f10
HTLA380 <htetlin099798@gmail.com>
2024-11-19 00:34:14 +0630

    chore(docs): update source file

--- apps/docs/lib/source.ts
+++ apps/docs/lib/source.ts
@@ -6,8 +6,3 @@ export const source = loader({
   baseUrl: "/docs",
   source: createMDXSource(docs, meta),
 })
-
-export const blog = loader({
-  baseUrl: "/blog",
-  source: createMDXSource(docs, meta),
-})
