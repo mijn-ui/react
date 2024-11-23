a36c450cfce11cd361498a6b5e425c7a828265ce
HTLA380 <htetlin099798@gmail.com>
2024-11-23 21:21:10 +0630

    feat: add progress package

---
+++ .changeset/funny-pigs-ring.md
@@ -0,0 +1,5 @@
+---
+"@mijn-ui-react/progress": patch
+---
+
+Add progress package
---
+++ packages/components/progress/src/progress.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import * as React from "react"
+import { applyUnstyled, UnstyledProps } from "@mijn-ui-react/utilities/shared"
+import * as ProgressPrimitive from "@radix-ui/react-progress"
+
+type ProgressProps = React.ComponentPropsWithRef<
+  typeof ProgressPrimitive.Root
+> &
+  UnstyledProps
+
+const Progress = ({
+  className,
+  unstyled,
+  value,
+  ref,
+  ...props
+}: ProgressProps) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={applyUnstyled(
+      unstyled,
+      "relative h-2 w-full overflow-hidden rounded-full bg-neutral",
+      className,
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className="size-full flex-1 bg-primary transition-all"
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    />
+  </ProgressPrimitive.Root>
+)
+
+export { Progress }
