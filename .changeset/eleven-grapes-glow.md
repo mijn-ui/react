48eb62f75f91cd2d84247f37367e8b36a77522cf
HTLA380 <htetlin099798@gmail.com>
2024-11-23 21:23:20 +0630

    feat: add separator package

---
+++ .changeset/eleven-grapes-glow.md
@@ -0,0 +1,5 @@
+---
+"@mijn-ui-react/separator": patch
+---
+
+Add separator package
---
+++ packages/components/separator/src/separator.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import * as React from "react"
+import { cn } from "@mijn-ui-react/utilities/shared"
+import * as SeparatorPrimitive from "@radix-ui/react-separator"
+
+type SeparatorProps = React.ComponentPropsWithRef<
+  typeof SeparatorPrimitive.Root
+>
+
+const Separator = ({
+  className,
+  orientation = "horizontal",
+  decorative = true,
+  ...props
+}: SeparatorProps) => (
+  <SeparatorPrimitive.Root
+    decorative={decorative}
+    orientation={orientation}
+    className={cn(
+      "shrink-0 bg-main-border",
+      orientation === "horizontal" ? "h-px w-full" : "h-full w-px",
+      className,
+    )}
+    {...props}
+  />
+)
+
+export { Separator }
