e6ceb0981b86ebb23fbe12d2669334a80062f3a7
HTLA380 <htetlin099798@gmail.com>
2024-11-28 16:52:54 +0630

    refactor: update component file paths and docs

--- packages/components/pagination/src/pagination.tsx
+++ packages/components/pagination/src/pagination.tsx
@@ -1,9 +1,71 @@
+"use client"
+
 import * as React from "react"
-import { usePaginationRange } from "./use-pagination"
 import { buttonStyles } from "@mijn-ui/react-button"
 import { UnstyledProvider, useUnstyled } from "@mijn-ui/react-utilities/context"
 import { UnstyledProps, applyUnstyled } from "@mijn-ui/react-utilities/shared"
 import { LuMoreHorizontal } from "react-icons/lu"
+
+/* -------------------------------------------------------------------------- */
+/*                             usePagination Hook                            */
+/* -------------------------------------------------------------------------- */
+type usePaginationProps = {
+  totalPages: number
+  itemsPerPage: number
+  currentPage: number
+}
+
+const usePaginationRange = ({
+  totalPages,
+  itemsPerPage,
+  currentPage,
+}: usePaginationProps) =>
+  React.useMemo(() => {
+    const totalPageCount = Math.ceil(totalPages / itemsPerPage)
+    const visiblePages = 3
+
+    if (totalPageCount <= visiblePages) {
+      return {
+        range: Array.from({ length: totalPageCount }, (_, i) => i + 1),
+        prevEllipsisActive: false,
+        nextEllipsisActive: false,
+      }
+    }
+
+    if (currentPage <= 2) {
+      return {
+        range: [1, 2, 3],
+        prevEllipsisActive: false,
+        nextEllipsisActive: totalPageCount > 3,
+      }
+    }
+
+    if (currentPage >= totalPageCount - 1) {
+      return {
+        range: [totalPageCount - 2, totalPageCount - 1, totalPageCount],
+        prevEllipsisActive: totalPageCount > 3,
+        nextEllipsisActive: false,
+      }
+    }
+
+    const leftBound = currentPage - 1
+    const rightBound = currentPage + 1
+
+    const range = Array.from(
+      { length: rightBound - leftBound + 1 },
+      (_, i) => i + leftBound,
+    )
+
+    return {
+      range,
+      prevEllipsisActive: leftBound > 1,
+      nextEllipsisActive: rightBound < totalPageCount,
+    }
+  }, [totalPages, itemsPerPage, currentPage])
+
+/* -------------------------------------------------------------------------- */
+/*                                 Pagination                                 */
+/* -------------------------------------------------------------------------- */
 
 type PaginationContextType = {
   paginationRange: number[]
