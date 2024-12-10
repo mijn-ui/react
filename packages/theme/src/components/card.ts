49f9bf72afce30657577208e1581981ed73bd1ed
HTLA380 <htetlin099798@gmail.com>
2024-12-10 22:02:32 +0630

    feat(packages|theme): add card styles

---
+++ packages/theme/src/components/card.ts
@@ -0,0 +1,15 @@
+import { tv, VariantProps } from "tailwind-variants"
+
+const cardStyles = tv({
+  slots: {
+    base: "bg-surface text-surface-text rounded-lg shadow-sm",
+    header: "flex flex-col space-y-1.5 p-4",
+    content: "p-4 pt-0",
+    footer: "flex items-center p-4 pt-0",
+    title: "text-2xl font-semibold leading-none tracking-tight",
+    description: "text-sm text-muted-text",
+  },
+})
+
+export type CardVariantProps = VariantProps<typeof cardStyles>
+export { cardStyles }
