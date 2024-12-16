b79455b3fabf6f5662690ed3126051dec51031d2
HTLA380 <htetlin099798@gmail.com>
2024-12-17 01:41:21 +0630

    chore: update opengraph image

--- apps/docs/app/og/[...slug]/route.tsx
+++ apps/docs/app/og/[...slug]/route.tsx
@@ -1,11 +1,31 @@
 import { generateOGImage } from "fumadocs-ui/og"
 import { metadataImage } from "@/lib/metadata-image"
+import Logo from "@/app/components/logo"
+import { readFileSync } from "fs"
+
+const font = readFileSync("./app/og/[...slug]/Geist-Regular.ttf")
+const fontBold = readFileSync("./app/og/[...slug]/Geist-Bold.ttf")
 
 export const GET = metadataImage.createAPI((page) => {
   return generateOGImage({
     title: page.data.title,
+    icon: <Logo width={60} height={60} />,
     description: page.data.description,
     site: "MijnUI",
+    primaryTextColor: "rgb(255,255,255)",
+    primaryColor: "rgba(249,115,22,0.5)",
+    fonts: [
+      {
+        name: "Geist",
+        data: font,
+        weight: 400,
+      },
+      {
+        name: "Geist",
+        data: fontBold,
+        weight: 600,
+      },
+    ],
   })
 })
 
